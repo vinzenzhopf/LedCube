e52f7a1975e9f4f40a96293b2990119ffb3bd08d
vhopf <vinzenz.hopf@hopf-hof.de>
2023-06-12 00:22:29 +0200

    Add a WIP of the WPFTimelineControl

---
+++ WpfTimelineControl/WpfTimelineControl.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace WpfTimelineControl.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ WpfTimelineControl/WpfTimelineControl/TimelineMark.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfTimelineControl
+{
+    /// <summary>
+    /// Interaction logic for TimelineMark.xaml
+    /// </summary>
+    public partial class TimelineMark : UserControl
+    {
+        public static readonly DependencyProperty PositionProperty = DependencyProperty.Register(
+            nameof (Position), typeof (int), typeof (TimelineMark),
+            new FrameworkPropertyMetadata(0));
+        
+        public static readonly DependencyProperty IsMajorTickProperty = DependencyProperty.Register(
+            nameof (IsMajorTick), typeof (bool), typeof (TimelineMark),
+            new FrameworkPropertyMetadata(false));
+
+        public int Position
+        {
+            get => (int) GetValue(PositionProperty);
+            set => SetValue(PositionProperty, value);
+        }
+        public bool IsMajorTick
+        {
+            get => (bool) GetValue(IsMajorTickProperty);
+            set => SetValue(IsMajorTickProperty, value);
+        }
+
+        public TimelineMark()
+        {
+            InitializeComponent();
+        }
+    }
+}
