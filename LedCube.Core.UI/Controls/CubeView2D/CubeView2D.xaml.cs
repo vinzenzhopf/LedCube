0a9182bd8254c9486a0a37d6d2144ba8452275e9
vhopf <vinzenz.hopf@hopf-hof.de>
2023-06-28 08:35:51 +0200

    Refactor CubeView2D control for better performance and readability
    In this commit, unnecessary properties were removed from the CubeView2D user control and ViewModel, resulting in a cleaner and more readable code structure, and also improving performance. The property change logging was also implemented for easier tracking of issues and a better debugging experience.
    
    Additionally, the Button commands in the CubeView2D.xaml were switched to RelayCommands and the layout was tweaked for visual improvements. PlaneData now triggers OnPlaneDataChanged, improving data binding reliability.
    
    Newly implemented RelayCommands were designed to cover the operations such as Clear, Set and Toggle on selected planes. This refactoring will allow for a more flexible and reliable operation of the 2D view of the LED cube in the application.
    
    Finally, minor changes to the MainWindow.xaml layout were made to better balance the allocation of space to various components of the UI.

--- LedCube.Core.UI/Controls/CubeView2D/CubeView2D.xaml.cs
+++ LedCube.Core.UI/Controls/CubeView2D/CubeView2D.xaml.cs
@@ -11,104 +11,10 @@ using LedCube.Core.Common.Model.Cube;
 
 namespace LedCube.Core.UI.Controls.CubeView2D;
 
-[ObservableObject]
 public partial class CubeView2D : UserControl
 {
-    public static readonly DependencyProperty GridWidthProperty = DependencyProperty.Register(
-        nameof(GridWidth), typeof(int), typeof(CubeView2D),
-        new FrameworkPropertyMetadata(8, OnGridDimensionsChanged));
-
-    public static readonly DependencyProperty GridHeightProperty = DependencyProperty.Register(
-        nameof(GridHeight), typeof(int), typeof(CubeView2D),
-        new FrameworkPropertyMetadata(8, OnGridDimensionsChanged));
-    
-    public static readonly DependencyProperty SelectedPlaneProperty = DependencyProperty.Register(
-        nameof(SelectedPlane), typeof(int), typeof(CubeView2D),
-        new FrameworkPropertyMetadata(0, OnGridDimensionsChanged));
-
-    private static void OnGridDimensionsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => 
-        (d as CubeView2D)?.OnGridDimensionsChanged(e);
-
-    public static readonly DependencyProperty LedBrushProperty = DependencyProperty.Register(
-        nameof(LedBrush), typeof(Brush), typeof(CubeView2D),
-        new FrameworkPropertyMetadata(Brushes.Blue));
-    private static void OnLedBrushChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => 
-        (d as CubeView2D)?.OnLedBrushChanged(e);
-
-    public static readonly DependencyProperty ShowNumbersProperty = DependencyProperty.Register(
-        nameof(ShowNumbers), typeof(bool), typeof(CubeView2D),
-        new FrameworkPropertyMetadata(true));
-
-    private static void OnShowNumbersChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => 
-        (d as CubeView2D)?.OnShowNumbersChanged(e);
-
-    public int GridWidth
-    {
-        get => (int)GetValue(GridWidthProperty);
-        set => SetValue(GridWidthProperty, value);
-    }
-    
-    public int GridHeight
-    {
-        get => (int)GetValue(GridHeightProperty);
-        set => SetValue(GridHeightProperty, value);
-    }
-    
-    public int SelectedPlane
-    {
-        get => (int)GetValue(SelectedPlaneProperty);
-        set => SetValue(SelectedPlaneProperty, value);
-    }
-    
-    public Brush LedBrush{
-        get => (Brush)GetValue(LedBrushProperty);
-        set => SetValue(LedBrushProperty, value);
-    }
-
-    public bool ShowNumbers{
-        get => (bool)GetValue(ShowNumbersProperty);
-        set => SetValue(ShowNumbersProperty, value);
-    }
-
-    [ObservableProperty]
-    private CubeConfig _cubeConfig;
-
-    [ObservableProperty]
-    private Plane<BiLed> _planeData;
-    
-    [ObservableProperty]
-    private Orientation3D _viewDirection = Orientation3D.Front;
-
-    public ObservableCollection<int> AllPlanes { get; } = new();
-    
-    public ObservableCollection<int> SelectedPlanes { get; } = new();
-
     public CubeView2D()
     {
         InitializeComponent();
-        for (var i = 0; i < 8; i++)
-        {
-            AllPlanes.Add(i);
-        }
-    }
-    
-    [RelayCommand]
-    private void SelectedItemsChanged(SelectionChangedEventArgs a)
-    {
-        
-    }
-    
-    private void OnGridDimensionsChanged(DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
-    {
-        // throw new System.NotImplementedException();
-    }
-    private void OnLedBrushChanged(DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
-    {
-        // throw new System.NotImplementedException();
-    }
-    
-    private void OnShowNumbersChanged(DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
-    {
-        // throw new System.NotImplementedException();
     }
 }
