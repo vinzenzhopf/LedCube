637d0a5cc6cfe3ca8e5bc58d55532b51ff7b3143
vhopf <vinzenz.hopf@hopf-hof.de>
2023-06-30 08:16:28 +0200

    Add UDP streaming service for LEDCube
    This commit introduces a new UDP streaming service to the LEDCube application. 
    New files are created to handle UDP datagram streaming, received data, utils, and service interfaces. 
    The commit encapsulates sending, receiving, and handling datagram functionality in the streaming service and improves the LEDCube's ability to stream data over a network. 
    Overall, it enhances networking capabilities and paves the way for remote LEDCube control and monitoring.

---
+++ LedCube.Streamer/Datagram/CubeDatagramUtils.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+using LedCube.Streamer.Service;
+
+namespace LedCube.Streamer.Datagram;
+
+public static class CubeDatagramUtils
+{
+    public static ReceivedDatagram ResolveDatagramContent(this UdpReceiveResult result)
+    {
+        var datagram = result.Buffer.AsSpan();
+        
+        var header = CubeDatagramHeader.ReadFromSpan(datagram[..CubeDatagramHeader.Size]);
+        var data = ParsePayloadData(header.PayloadType, datagram[CubeDatagramHeader.Size..]);
+        
+        return new ReceivedDatagram(result.RemoteEndPoint, header, data);
+    }
+    
+    public static object? ParsePayloadData(DatagramType type, ReadOnlySpan<byte> payloadSpan)
+    {
+        return type switch
+        {
+            DatagramType.Discovery => InfoResponsePayload.ReadFromSpan(payloadSpan),
+            DatagramType.InfoResponse => InfoResponsePayload.ReadFromSpan(payloadSpan),
+            DatagramType.ErrorResponse => InfoResponsePayload.ReadFromSpan(payloadSpan),
+            DatagramType.AnimationStartAck => AnimationStartResponsePayload.ReadFromSpan(payloadSpan),
+            DatagramType.AnimationEndAck => AnimationEndResponsePayload.ReadFromSpan(payloadSpan),
+            DatagramType.FrameDataAck => FrameResponsePayload.ReadFromSpan(payloadSpan),
+            _ => null
+        };
+    }
+}
---
+++ LedCube.Streamer/Service/UdpStreamerService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using LedCube.Core.Common.Config;
+using LedCube.Core.CubeData.Repository;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace LedCube.Streamer.Service;
+
+public class UdpStreamerService : BackgroundService
+{
+    private ILogger Logger { get; }
+    private readonly ICubeConfigRepository _cubeConfigRepository;
+    private readonly ICubeRepository _cubeRepository;
+
+    private UdpClient _udpClient;
+    private CancellationTokenSource _receiverCancellationTks;
+    private Task _receiverTask;
+
+    public UdpStreamerService(ILoggerFactory loggerFactory, ICubeConfigRepository cubeConfigRepository, ICubeRepository cubeRepository)
+    {
+        Logger = loggerFactory.CreateLogger(GetType());
+        _cubeConfigRepository = cubeConfigRepository;
+        _cubeRepository = cubeRepository;
+    }
+    
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _udpClient = new UdpClient(4242);
+            
+            
+            _receiverCancellationTks = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken);
+            // _receiverTask = Task.Run(() => ReceiveMessagesAsync(_receiverCancellationTks.Token));
+            
+            
+        }
+
+        return Task.CompletedTask;
+    }
+}
