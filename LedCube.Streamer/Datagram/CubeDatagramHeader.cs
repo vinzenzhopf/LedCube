5986700466046d5c5670d050dfdbef20f24dcf20
vhopf <vinzenz.hopf@hopf-hof.de>
2023-06-30 08:15:41 +0200

    Rename Header to CubeDatagramHeader for clarity
    Renamed struct and file name from Header to CubeDatagramHeader in the LedCube.Streamer.Datagram namespace to better reflect its purpose and functionality. This change ensures a greater understanding of the codebase and improves readability.

--- LedCube.Streamer/Datagram/CubeDatagramHeader.cs
+++ LedCube.Streamer/Datagram/CubeDatagramHeader.cs
@@ -2,27 +2,27 @@
 
 namespace LedCube.Streamer.Datagram;
 
-public struct Header
+public struct CubeDatagramHeader
 {
     public DatagramType PayloadType;
     public UInt16 PacketCount;
 
     public static int Size => sizeof(DatagramType) + sizeof(UInt16);
     
-    public static Header ReadFromSpan(ReadOnlySpan<byte> span)
+    public static CubeDatagramHeader ReadFromSpan(ReadOnlySpan<byte> span)
     {
-        return new Header()
+        return new CubeDatagramHeader()
         {
             PayloadType = MemoryMarshal.Read<DatagramType>(span[0..]),
             PacketCount = MemoryMarshal.Read<UInt16>(span[2..]),
         };
     }
 
-    public static ReadOnlySpan<byte> WriteToSpan(Header header)
+    public static ReadOnlySpan<byte> WriteToSpan(CubeDatagramHeader cubeDatagramHeader)
     {
         var span = new byte[Size].AsSpan();
-        MemoryMarshal.Write(span[0..], ref header.PayloadType);
-        MemoryMarshal.Write(span[2..], ref header.PacketCount);
+        MemoryMarshal.Write(span[0..], ref cubeDatagramHeader.PayloadType);
+        MemoryMarshal.Write(span[2..], ref cubeDatagramHeader.PacketCount);
         return span;
     }
 
