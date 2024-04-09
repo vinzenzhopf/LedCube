71ba6060ef35c75275cdadff2e5660c897ac38d7
vhopf <vinzenz.hopf@hopf-hof.de>
2024-04-09 19:08:27 +0200

    Ascii output for SdfTest

--- LedCube.Test/Core/SdfTests/SdfTest.cs
+++ LedCube.Test/Core/SdfTests/SdfTest.cs
@@ -1,8 +1,11 @@
 ﻿using System.Diagnostics;
 using System.Numerics;
+using System.Text;
 using LedCube.Core.Common.CubeData.Generator;
+using LedCube.Core.Common.Model;
 using LedCube.Core.Common.Model.Cube;
 using LedCube.Core.Common.Model.Cube.Buffer;
+using LedCube.Sdf.Core;
 using Xunit.Abstractions;
 
 namespace LedCube.Test.Core.SdfTests;
@@ -45,4 +48,96 @@ public class SdfTest
         var time = sw.Elapsed;
         _testOutputHelper.WriteLine("Elapsed Time: {0}", time);
     }
+    
+    [Fact]
+    public void TestSdfFrame()
+    {
+        ICubeData buffer = new CubeData<CubeDataBuffer16>();
+        buffer.Clear();
+        
+        var sut = Sdf.Core.Sdf.BoxFrame(new Vector3(4, 4, 4), 0.25f);
+        
+        
+        buffer.Render(sut, 0, new SdfRenderOptions(){Centered = true, Margin = 0.499f});
+        var sb = new StringBuilder();
+        sb.AppendCubeData(buffer);
+        _testOutputHelper.WriteLine(sb.ToString());
+    }
+}
+
+public static class SdfTestOutputExtensions
+{
+    public static StringBuilder AppendCubeData(this StringBuilder sb, ICubeData cubeData)
+    {
+        const int stackCount = 4;
+        for (var z = 0; z < cubeData.Size.Z; z+=stackCount)
+        {
+            sb.AppendStackedPlaneData(cubeData, z, stackCount);
+        }
+        return sb;
+    }
+
+    public static StringBuilder AppendStackedPlaneData(this StringBuilder sb, ICubeData cubeData, int planeIndex, int planeCount = 1)
+    {
+        //Header
+        for (var i = 0; i < planeCount; i++)
+        {
+            sb.Append($"Plane {planeIndex+i:D2} Data:");
+            var missing = 3 + 2 * cubeData.Size.X - 14;
+            for (var s = 0; s < missing; s++)
+            {
+                sb.Append(' ');
+            }
+            sb.Append("\t\t");
+        }
+        sb.Append(" \n");
+        for (var i = 0; i < planeCount; i++)
+        {
+            sb.AppendPlaneHeader(cubeData.Size.X);
+        }
+        sb.Append(" \n");
+        
+        
+        for (var y = 0; y < cubeData.Size.Y; y++)
+        {
+            for (var i = 0; i < planeCount; i++)
+            {
+                sb.Append(NumberToSingleChar(y));
+                for (var x = 0; x < cubeData.Size.X; x++)
+                {
+                    sb.Append(' ').Append(cubeData.GetLed(new Point3D(x, y, planeIndex + i)) ? 'X' : ' ');
+                }
+                sb.Append(' ');
+                sb.Append(NumberToSingleChar(y));
+                sb.Append("\t\t");    
+            }
+            sb.Append('\n');
+        }
+        
+        //Footer Row
+        for (var i = 0; i < planeCount; i++)
+        {
+            sb.AppendPlaneHeader(cubeData.Size.X);
+        }
+        sb.Append(" \n");
+        return sb;
+    }
+
+    private static StringBuilder AppendPlaneHeader(this StringBuilder sb, int size)
+    {
+        sb.Append(' ');
+        for (var x = 0; x < size; x++)
+        {
+            sb.Append(' ').Append(NumberToSingleChar(x));
+        }
+        sb.Append("  ");
+        sb.Append("\t\t");
+        return sb;
+    }
+    
+    private static char NumberToSingleChar(int index)
+    {
+        const string map = "0123456789ABCDEFGHIJKLMNOPQRSTUV";
+        return (index < 0 || index > map.Length) ? '-' : map[index];
+    } 
 }
